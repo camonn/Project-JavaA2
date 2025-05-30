bf1cdd42bc370b7a4b273241825c95e8d3e9528c
camonn <camonnritse18@gmail.com>
2025-05-29 22:25:38 -0300

    first commit

---
+++ javauniesp/src/main/java/com/parking/javauniesp/JavauniespApplication.java
@@ -0,0 +1,13 @@
+package com.parking.javauniesp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavauniespApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavauniespApplication.class, args);
+	}
+
+}
---
+++ javauniesp/src/test/java/com/parking/javauniesp/JavauniespApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parking.javauniesp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavauniespApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
